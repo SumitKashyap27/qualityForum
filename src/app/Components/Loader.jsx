504c6be7a288c386095843ac25075f704cff0cee
SumitKashyap27 <sumit20020927@gmail.com>
2023-12-04 19:37:34 +0530

    Loader Added

---
+++ src/app/Components/Loader.jsx
@@ -0,0 +1,12 @@
+"use client"
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export default function loader() {
+  return (
+    <Box sx={{ display: 'flex', paddingTop:20 }}>
+      <CircularProgress sx={{color:'whitesmoke'}}/>
+    </Box>
+  );
+}

7d118d53573597c0d213dbcb550c893e1984cba5
mrtechnostart <rambpandey238@gmail.com>
2023-12-06 16:54:28 +0530

    Create Student

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -2,7 +2,18 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const token = await getToken({ req: req });
-  console.log(token);
-  return NextResponse.json(token);
+  try {
+    const token = await getToken({ req: req });
+    const data = await req.json();
+    if (token && token.user.id) {
+      const response = await prisma.user.create({
+        data: data,
+      });
+      return NextResponse.json({ data: response }, { status: 200 });
+    }
+    return NextResponse.json({ data: "unauthorized" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }

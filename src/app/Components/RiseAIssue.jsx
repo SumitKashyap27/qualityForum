26ecaec0974968d2e9f1b85a92c57dade44c0d5e
mrtechnostart <rambpandey238@gmail.com>
2023-12-07 22:07:20 +0530

    RaiseAIssue

--- src/app/Components/RiseAIssue.jsx
+++ src/app/Components/RiseAIssue.jsx
@@ -1,11 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import {
-  InternetPieChart,
-  LaundryPieChart,
-  MessPieChart,
-  SweeperPieChart,
-} from "./Charts";
+import { Popup, failToast, successToast } from "./SuccessPopup";
 import axios from "axios";
 
 const plans = [
@@ -26,226 +21,91 @@ const plans = [
   },
 ];
 
-const ViewDashboard = (props) => {
-  const { reviews } = props;
+export default function RaiseAIssue() {
   const [selectedPlans, setSelectedPlans] = useState([]);
-  const [comments, setComments] = useState([]);
-  const [raiseData, setRaiseData] = useState([]);
+  const [feedbackData, setFeedbackData] = useState({
+    type: null,
+    satisfied: null,
+  });
 
   useEffect(() => {
-    // Fetch comments from the server and update the state
-    const fetchData = async () => {
+    // Log the feedback data whenever it changes
+    console.log("Feedback Data:", feedbackData);
+    (async () => {
       try {
-        const { data } = await axios.get("/api/issue");
-        setComments(data.data);
+        const response = await axios.post("/api/raise", feedbackData);
+        if (response) {
+          successToast("Successfull");
+        }
       } catch (error) {
-        console.error("Error fetching comments:", error);
+        console.log(error);
       }
-    };
+    })();
+  }, [feedbackData]);
 
-    fetchData();
+  const togglePlan = (plan) => {
+    if (selectedPlans.includes(plan)) {
+      setSelectedPlans(selectedPlans.filter((selected) => selected !== plan));
+    } else {
+      setSelectedPlans([...selectedPlans, plan]);
+    }
+  };
 
-    // Fetch raise data from the server and update the state
-    const fetchRaiseData = async () => {
-      try {
-        const { data } = await axios.get("/api/raise");
-        setRaiseData(data.data);
-      } catch (error) {
-        console.error("Error fetching raise data:", error);
-      }
-    };
-
-    fetchRaiseData();
-  }, []);
-
-  const calculateSatisfaction = (category) => {
-    const categoryData = raiseData.filter((item) => item.type === category);
-    const satisfiedCount = categoryData.filter((item) => item.satisfied).length;
-    const unsatisfiedCount = categoryData.filter(
-      (item) => !item.satisfied
-    ).length;
-
-    return { satisfied: satisfiedCount, unsatisfied: unsatisfiedCount };
+  const handleFeedback = (feedbackType, isSatisfied) => {
+    setFeedbackData({
+      type: feedbackType,
+      satisfied: isSatisfied,
+    });
   };
 
   return (
-    <main>
-      <header className=" bg-opacity-60 self-stretch flex flex-col mb-1 pb-24 px-5 max-md:max-w-full">
-        <section className="self-center w-full mt-4 max-md:max-w-full max-md:mt-2">
-          <div className="gap-3 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-            <div className="flex flex-col drop-shadow-xl items-stretch w-3/12 max-md:w-full max-md:ml-0">
-              <button
-                className={
-                  "flex w-full grow flex-col flex-1 mx-auto px-3 py-6 rounded-xl max-md:mt-2 shadow-sm bg-white text-black transform scale-100 transition-transform"
-                }
-              >
-                <h2 className="text-black text-2xl self-center whitespace-nowrap">
-                  MESS FOOD
-                </h2>
-                <div className="aspect-w-16 aspect-h-9 object-contain object-center overflow-hidden self-center mt-4">
-                  <p>Graph of mess food</p>
-                  {reviews.length !== 0 && (
-                    <MessPieChart MessData={reviews.MESS} />
-                  )}
-                </div>
-                <div className="mt-4">
-                  <p>
-                    Total Satisfied: {calculateSatisfaction("MESS").satisfied}
-                  </p>
-                  <p>
-                    Total Unsatisfied:{" "}
-                    {calculateSatisfaction("MESS").unsatisfied}
-                  </p>
-                </div>
-              </button>
-            </div>
-            <div className="flex drop-shadow-xl flex-col items-stretch w-3/12 ml-2 max-md:w-full max-md:ml-0">
-              <button
-                className={
-                  "flex w-full grow flex-col flex-1 mx-auto px-3 py-6 rounded-xl max-md:mt-2 shadow-sm bg-white text-black transform scale-100 transition-transform"
-                }
-              >
-                <h2 className="text-black text-2xl self-center whitespace-nowrap">
-                  SWEEPER
-                </h2>
-                <div className="aspect-w-16 aspect-h-9 object-contain object-center overflow-hidden self-center mt-4">
-                  <p>Graph of Sweeper Service</p>
-                  {reviews.length !== 0 && (
-                    <SweeperPieChart Sweeper={reviews.SWEEPER} />
-                  )}
-                </div>
-                <div className="mt-4">
-                  <p>
-                    Total Satisfied:{" "}
-                    {calculateSatisfaction("SWEEPER").satisfied}
-                  </p>
-                  <p>
-                    Total Unsatisfied:{" "}
-                    {calculateSatisfaction("SWEEPER").unsatisfied}
-                  </p>
-                </div>
-              </button>
-            </div>
-            <div className="flex drop-shadow-xl flex-col items-stretch w-3/12 ml-2 max-md:w-full max-md:ml-0">
-              <button
-                className={
-                  "flex w-full grow flex-col flex-1 mx-auto px-3 py-6 rounded-xl max-md:mt-2 shadow-sm bg-white text-black transform scale-100 transition-transform"
-                }
-              >
-                <h2 className="text-black text-2xl self-center whitespace-nowrap">
-                  LAUNDRY
-                </h2>
-                <div className="aspect-w-16 aspect-h-9 object-contain object-center overflow-hidden self-center mt-4">
-                  <p>Graph of Laundry Service</p>
-                  {reviews.length !== 0 && (
-                    <LaundryPieChart LaundryData={reviews.Laundry} />
-                  )}
-                </div>
-                <div className="mt-4">
-                  <p>
-                    Total Satisfied:{" "}
-                    {calculateSatisfaction("LAUNDRY").satisfied}
-                  </p>
-                  <p>
-                    Total Unsatisfied:{" "}
-                    {calculateSatisfaction("LAUNDRY").unsatisfied}
-                  </p>
-                </div>
-              </button>
-            </div>
-            <div className="flex flex-col drop-shadow-xl items-stretch w-3/12 ml-2 max-md:w-full max-md:ml-0">
-              <button
-                className={
-                  "flex w-full grow flex-col flex-1 mx-auto px-3 py-6 rounded-xl max-md:mt-2 shadow-sm bg-white text-black"
-                }
-              >
-                <h2 className="text-black text-2xl self-center whitespace-nowrap">
-                  INTERNET
-                </h2>
-                <div className="aspect-w-16 aspect-h-9 object-contain object-center overflow-hidden self-center mt-4">
-                  <p>Graph of Internet Service</p>
-                  {reviews.length !== 0 && (
-                    <InternetPieChart Internet={reviews.INTERNET} />
-                  )}
-                </div>
-                <div className="mt-4">
-                  <p>
-                    Total Satisfied:{" "}
-                    {calculateSatisfaction("INTERNET").satisfied}
-                  </p>
-                  <p>
-                    Total Unsatisfied:{" "}
-                    {calculateSatisfaction("INTERNET").unsatisfied}
-                  </p>
-                </div>
-              </button>
-            </div>
-          </div>
-        </section>
-        <section>
-          <>
-            <div className="grid grid-cols-3 md:grid-cols-2 gap-4 drop-shadow-xl mt-14 max-md:max-w-full max-md:mt-10">
-              <div className="grid  items-stretch max-md:w-full max-md:ml-0">
-                <div className="shadow-sm bg-white flex w-full shrink-0 h-auto flex-col mx-auto rounded-xl max-md:max-w-full max-md:mt-9">
-                  <div className="grid col-span-2  md:col-span-2 w-full items-start gap-4 px-4 py-10 mx-auto">
-                    <h1 className="flex items-center text-2xl">View Issues</h1>
-                    {plans.map((plan) => (
-                      <React.Fragment key={plan.name}>
-                        <div
-                          className={`relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none ${
-                            selectedPlans.includes(plan)
-                              ? "bg-red-900/75 text-white"
-                              : "bg-green-500"
-                          }`}
-                          onClick={() => togglePlan(plan)}
-                        >
-                          <div className="flex w-full items-center justify-between">
-                            <div className="flex items-center">
-                              <div className="text-sm">
-                                <p
-                                  className={`font-medium ${
-                                    selectedPlans.includes(plan)
-                                      ? "text-white"
-                                      : "text-gray-900"
-                                  }`}
-                                >
-                                  {plan.name}
-                                </p>
-                                {/* Description can be added here */}
-                              </div>
-                            </div>
-                            {selectedPlans.includes(plan) && (
-                              <div className="shrink-0 text-white"></div>
-                            )}
-                          </div>
-                        </div>
-                      </React.Fragment>
-                    ))}
+    <>
+      <h1 className="flex items-center text-4xl">Share your Issues</h1>
+      <div className="grid justify-center items-center grid-cols-2 gap-4 px-4 py-10 mx-auto">
+        {plans.map((plan) => (
+          <React.Fragment key={plan.name}>
+            <div
+              className={`relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none ${
+                selectedPlans.includes(plan)
+                  ? "bg-red-900/75 text-white"
+                  : "bg-white"
+              }`}
+              onClick={() => togglePlan(plan)}
+            >
+              <div className="flex w-full items-center justify-between">
+                <div className="flex items-center">
+                  <div className="text-sm">
+                    <p
+                      className={`font-medium ${
+                        selectedPlans.includes(plan)
+                          ? "text-white"
+                          : "text-gray-900"
+                      }`}
+                    >
+                      {plan.name}
+                    </p>
                   </div>
                 </div>
               </div>
-              <div className="grid col-span-1 md:col-span-1 flex-grow items-stretch ml-0 md:ml-5 max-md:w-full max-md:ml-0 bg-white shadow-sm rounded-xl pt-10 pl-5">
-                <p>Comments/Suggestions</p>
-                <div className="pr-6 flex w-full flex-col rounded-sm self-start max-md:max-w-full overflow-y-auto max-h-80">
-                  {comments.map((element, index) => (
-                    <div key={index} className="mb-4">
-                      <p>
-                        {element.createdAt.slice(0, 10) +
-                          " " +
-                          element.createdAt.slice(11, 16)}
-                      </p>
-                      <p>{element.type}</p>
-                      <p>{element.comment}</p>
-                    </div>
-                  ))}
-                </div>
-              </div>
             </div>
-          </>
-        </section>
-      </header>
-    </main>
+            <div className="flex items-center">
+              <button
+                onClick={() => handleFeedback(plan.name, true)}
+                className="inline-flex items-center px-4 py-2 font-semibold tracking-tighter transition duration-500 ease-in-out transform bg-green-500 border rounded-lg text-md hover:text-white hover:bg-green-600 focus:shadow-outline"
+              >
+                Satisfied
+              </button>
+              <button
+                onClick={() => handleFeedback(plan.name, false)}
+                className="inline-flex items-center px-4 py-2 font-semibold tracking-tighter transition duration-500 ease-in-out transform bg-red-500 border rounded-lg text-md hover:text-white hover:bg-red-600 focus:shadow-outline"
+              >
+                Unsatisfied
+              </button>
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+      <Popup />
+    </>
   );
-};
-
-export default ViewDashboard;
+}

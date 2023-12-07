8148f667e74a7de395e96d98be6ca0f5098ebdba
SumitKashyap27 <sumit20020927@gmail.com>
2023-12-07 17:47:58 +0530

    added colors to graph

--- src/app/Components/SysAdminView.jsx
+++ src/app/Components/SysAdminView.jsx
@@ -26,11 +26,11 @@ const SysAdminView = (props) => {
         <div className="flex w-full gap-2 max-md:flex-col max-md:items-stretch max-md:gap-0">
           <div className="flex flex-col items-stretch border rounded-xl border-solid border-black w-[59%] max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col items-stretch max-md:max-w-full max-md:mt-8">
-              <div className=" pl-9 flex-col text-black text-3xl shadow-sm bg-white rounded-xl leading-9 relative fill-white overflow-hidden min-h-[600px] w-full items-center pr-5 pt-8 pb-[478px] max-md:max-w-full max-md:pb-10">
+              <div className=" pl-9 flex-col text-black text-3xl shadow-sm bg-white rounded-xl leading-9 relative fill-white overflow-hidden w-full items-center pr-5 pt-8 pb-10 max-md:max-w-full max-md:pb-10">
                 INTERNET
                 {reviews && (
                   <InternetPieChart
-                    className="mt-7"
+                    className="mt-40"
                     Internet={reviews.INTERNET}
                   />
                 )}
@@ -39,11 +39,10 @@ const SysAdminView = (props) => {
           </div>
           <div className="flex flex-col items-stretch w-[41%] ml-5 max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col items-stretch  max-md:max-w-full max-md:mt-10">
-              <div className="border shadow-sm bg-white flex flex-col items-stretch pl-9 pr-8 py-8 rounded-xl min-h-[600px] border-solid border-black max-md:max-w-full max-md:mt-10 max-md:px-5">
-                <div className="text-neutral-700 text-xl leading-7 max-md:max-w-full">
+              <div className="border shadow-sm bg-white flex flex-col items-stretch pl-9 pr-8 py-8 rounded-xl h-auto border-solid border-black max-md:max-w-full max-md:mt-10 max-md:px-5">
+                <div className="text-neutral-700 text-xl leading-7 max-md:max-w-full h-auto">
                   Comments
-                </div>
-                <div className="border flex shrink-0 rounded h-[590px] border-solid border-black flex-col mt-6 max-md:max-w-full" />
+                <div className="border p-6 flex shrink-0 rounded h-[590px] border-solid border-black overflow-auto flex-col mt-6 max-md:max-w-full" >
                 {comments.map((element, index) => (
                   <div key={index} className="mb-4">
                     <p>
@@ -55,6 +54,8 @@ const SysAdminView = (props) => {
                     <p>{element.comment}</p>
                   </div>
                 ))}
+                </div>
+                  </div>
               </div>
             </div>
           </div>
